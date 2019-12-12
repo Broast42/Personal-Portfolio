323aa461278def0963958bce24fc4d175410cbb2
rdowd <rjarrettdowd@yahoo.com>
2019-12-11 19:20:38 -0500

    added coponents for nav and footer

--- js/index.js
+++ js/index.js
@@ -11,23 +11,23 @@ window.addEventListener("load", ()=>{
 
 //hover over links and rotate mini-logos
 
-const linkDiv = document.querySelectorAll(".link-container");
+// const linkDiv = document.querySelectorAll(".link-container");
 
-const miniLogo = document.querySelectorAll('.link-logo');
+// const miniLogo = document.querySelectorAll('.link-logo');
 
-linkDiv.forEach((x, i) =>{
-    x.addEventListener("mouseenter", ()=>{
-        miniLogo[i].style.transform = "rotate(360deg)";
-        miniLogo[i].style.transition = "transform 3s";
+// linkDiv.forEach((x, i) =>{
+//     x.addEventListener("mouseenter", ()=>{
+//         miniLogo[i].style.transform = "rotate(360deg)";
+//         miniLogo[i].style.transition = "transform 3s";
         
-    });
+//     });
 
-    x.addEventListener("mouseleave", ()=>{
-        miniLogo[i].style.transform = "";
-        miniLogo[i].style.transition = "";
+//     x.addEventListener("mouseleave", ()=>{
+//         miniLogo[i].style.transform = "";
+//         miniLogo[i].style.transition = "";
         
-    });
-});
+//     });
+// });
 
 
 // randomly pick a p element and fade it in and out when entering a div
@@ -66,4 +66,140 @@ allSectionP.forEach(x=>{
         x.style.fontFamily = "";
     });
 });
+
+//header link data (edit this to edit nav links)
+
+const navData = [
+    {
+        name: "Home",
+        source: "index.html"
+    },
+    {
+        name: "About Me",
+        source: "about.html"
+    },
+    {
+        name: "Skills",
+        source: "#",
+    },
+    {
+        name: "Projects",
+        source: "#",
+    },
+];
+
+//nav bar
+
+                    // <nav>
+                    //     <div class="link-container">
+                    //         <div class="link-logo">
+                    //             <div class="link-logo-content"></div>
+                    //         </div>
+                    //         <a href="index.html">Home</a>
+                    //     </div>
+                    //     <div class="link-container">
+                    //         <div class="link-logo">
+                    //             <div class="link-logo-content"></div>
+                    //         </div>
+                    //         <a href="#">About Me</a>
+                    //     </div >
+                    //     <div class="link-container">
+                    //         <div class="link-logo">
+                    //             <div class="link-logo-content"></div>
+                    //         </div>
+                    //         <a href="#">Skills</a>
+                    //     </div>
+                    //     <div class="link-container">
+                    //         <div class="link-logo">
+                    //             <div class="link-logo-content"></div>
+                    //         </div>
+                    //         <a href="#">Projects</a>
+                    //     </div>    
+                    // </nav>
+
+const navBarDiv = document.querySelector("#nav");
+navBarDiv.appendChild(createNav(navData));
+
+function createNav(arr){
+    const nav = document.createElement("nav");
+
+    arr.forEach(x =>{
+        const container = document.createElement("div");
+        const logo = document.createElement("div");
+        const logoContent = document.createElement("div");
+        const link = document.createElement("a");
+
+        container.classList.add("link-container");
+        logo.classList.add("link-logo");
+        logoContent.classList.add("link-logo-content");
+        link.src = x.source;
+        link.textContent = x.name;
+
+        nav.appendChild(container);              
+        container.appendChild(logo);
+        logo.appendChild(logoContent);
+        container.appendChild(link);
+
+        container.addEventListener("mouseenter", ()=>{
+            logo.style.transform = "rotate(360deg)";
+            logo.style.transition = "transform 3s";
+                     
+        });
+
+        container.addEventListener("mouseleave", ()=>{
+            logo.style.transform = "";
+            logo.style.transition = "";
+        });
+    });
+
+    return nav;
+
+}
+
+//footer data object (edit this to edit footer links)
+
+const footerData = [
+    {
+        source: "#",
+        class1: "fab",
+        class2: "fa-linkedin" 
+    },
+    {
+        source: "#",
+        class1: "fab",
+        class2: "fa-facebook-square" 
+    },
+    {
+        source: "#",
+        class1: "fab",
+        class2: "fa-twitter-square" 
+    },
+
+];
+
+
+const footer = document.querySelector("footer");
+
+footer.appendChild(createFooter(footerData));
+
+
+function createFooter(arr){
+    const div = document.createElement("div");
+
+    arr.forEach(x=>{
+        const link = document.createElement('a');
+        const icon = document.createElement('i');
+
+        icon.classList.add(x.class1, x.class2);
+        link.src = x.source;
+
+        link.appendChild(icon);
+        div.appendChild(link);
+
+    });
+
+    return div;
+    
+}
+
 
